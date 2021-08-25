e41932e60813cafbaf17011987c8678ac5f7d14d
waiting <1661926154@qq.com>
2021-08-25 19:30:45 +0800

    test: add 13.knex.test.ts

---
+++ packages/demo/test/13.knex.test.ts
@@ -0,0 +1,62 @@
+import { basename } from '@waiting/shared-core'
+
+import {
+  OnQueryData,
+  OnQueryErrorData,
+  OnQueryErrorErr,
+  OnQueryRespRaw,
+  QueryResponse,
+} from '../src/index'
+
+import { inst } from './test.config'
+
+// eslint-disable-next-line import/order
+import assert = require('power-assert')
+
+
+const filename = basename(__filename)
+
+describe(filename, () => {
+
+  after(async () => {
+    await inst.destroy()
+  })
+
+  describe('Should read table with tables param in object works', () => {
+    it('tb_user', async () => {
+      const knex = inst
+      const trx = await knex.transaction()
+
+      await trx('tb_user')
+        .select('*')
+        .on('query', (data: OnQueryData) => {
+          console.log('00:', data.__knexTxId)
+          // assert(! data.__knexTxId)
+        })
+
+      await trx('tb_user')
+        .transacting(trx)
+        // .forUpdate()
+        .select('*')
+        .where('uid', 1)
+        .on('query', (data: OnQueryData) => {
+          console.log('01:', data.__knexTxId)
+          // assert(data.__knexTxId) // 'trx2'
+        })
+        .then(async (rows) => {
+          await trx.commit()
+          return rows
+        })
+
+      await trx('tb_user')
+        .select('*')
+        .on('query', (data: OnQueryData) => {
+          console.log('02:', data.__knexTxId)
+          // assert(! data.__knexTxId)
+        })
+
+    })
+
+  })
+})
+
